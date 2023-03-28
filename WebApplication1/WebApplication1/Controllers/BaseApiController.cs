2ebbb681bc5dfc1d15cec55d2405439bef742987
ali <ali@gmail.com>
2023-03-28 22:59:51 +0500

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/BaseApiController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        public static string ValidateToken(string token)
+        {
+            if (token == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Convert.ToString(ConfigurationManager.AppSettings["config:JwtKey"]));
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var jti = jwtToken.Claims.First(claim =>claim.Type == "jti").Value;
+                var userName = jwtToken.Claims.First(sub => sub.Type == "sub").Value;
+
+                // return user id from JWT token if validation successful
+                return userName;
+            }
+            catch
+            {
+                // return null if validation fails
+                return null;
+            }
+        }
+
+    }
+}
